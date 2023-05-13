a3c7a08931084b43b2c12335d902a03a40627dd6
fhk2022 <121293074+fhk2022@users.noreply.github.com>
2023-05-14 00:06:51 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "production-server" {
  ami           = "ami-02eb7a4783e7e9317" 
  instance_type = "t2.micro"
  availability_zone = "ap-south-1"
- VPC_security_groups = [aws_security_group.ec2group123.id]
  key_name = "dec3batch"
  tags = {
  name = "ansible_instance"
