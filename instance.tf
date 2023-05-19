5ac9b03b698cbc5057924ceddc2250590c7ead6c
fhk2022 <121293074+fhk2022@users.noreply.github.com>
2023-05-20 01:04:09 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "production-server" {
  ami           = "ami-02eb7a4783e7e9317" 
  instance_type = "t2.micro"
  availability_zone = "ap-south-1"
- key_name = "dec3batch"
+ key_name = "Project2"
  tags = {
  name = "ansible_instance"
  }
