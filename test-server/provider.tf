4168b7126e3c9533aab4d36681d5f3cf017c3a30
fhk2022 <121293074+fhk2022@users.noreply.github.com>
2023-05-20 01:00:16 +0530

    Update provider.tf

--- test-server/provider.tf
+++ test-server/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 region     = "ap-south-1" 
-access_key= "AKIAQTTRSJCEOWRZVNH2"
-secret_key= "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" 
+access_key= "AKIASFIBWFZZ4P5BQNLM"
+secret_key= "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" 
  
 }
